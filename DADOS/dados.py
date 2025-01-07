b20b31db3238bd1a5ce97c2f91af2228ed13bb4a
Caroline Oliveira <caroline@pet-si.ufrrj.br>
2025-01-07 15:17:05 -0300

    Tratamento dos valores inseridos

--- DADOS/dados.py
+++ DADOS/dados.py
@@ -1,9 +1,28 @@
 #	lançamento de x dados de n faces
 import random
 print("Bem-vindo(a) ao lançamento de dados versão 2.0")
+print(" ")
 
-x = int(input("Quantos dados você deseja lançar? "))    #Quantidade de dados lançados de uma vez
-n = int(input("Quantas faces possui o dado que deseja lançar? "))   #Número de faces que o dado terá sendo 6 o número mínimo de faces de um dado
+
+while True:
+    x = int(input("Quantos dados você deseja lançar? "))    #Quantidade de dados lançados de uma vez
+    if x > 0:
+        print("Número válido!")
+        break
+    else:
+        print("Número inválido. Digite um número maior que 0.")
+
+
+print("Para este jogo são considerados dados com 6 ou mais faces.")
+
+while True:
+    n = int(input("Quantas faces possui o dado que deseja lançar? "))
+
+    if n >= 6:
+        print("Número válido!")
+        break
+    else:
+        print("Número inválido. Digite um número maior ou igual a 6.")
 
 lancamento = 1
 
